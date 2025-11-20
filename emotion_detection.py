76263026fdc3b84bcfd215ba900c74134bd97865
mahesh.km <mahesh.km@feathersoft.com>
2025-11-20 22:39:18 +0530

    emotion_detection format changes

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,12 @@
 import requests
-
+import json
 
 def emotion_detector(text_to_analyse):
     url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header=  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     query={ "raw_document": { "text": text_to_analyse } }
     response=requests.post(url,json=query,headers=header)
-    return response.text
+    data= json.loads(response.text)['emotionPredictions'][0]['emotion']
+    max_key = max(data, key=data.get)
+    data['dominant_emotion']= max_key
+    return data
